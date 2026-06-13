0c02e00335b529875cdfa685d4cc2c0dbb9da738
dowiyogo <dowiyogo@gmail.com>
2026-06-14 00:33:54 +0200

    build: GDML opcional (compila sin GDML)

--- tests/export_endonly_gdml.cc
+++ tests/export_endonly_gdml.cc
@@ -1,23 +1,30 @@
 #include "DetectorConstruction.hh"
 
+#ifdef USE_GDML
 #include "G4GDMLParser.hh"
+#endif
 
 #include <cstdlib>
 #include <iostream>
 
 int main(int argc, char** argv) {
+#ifdef USE_GDML
     if (argc != 2) {
-        std::cerr << "usage: export_endtop_gdml OUTPUT.gdml\n";
+        std::cerr << "usage: export_endonly_gdml OUTPUT.gdml\n";
         return EXIT_FAILURE;
     }
 
     DetectorConstruction detector;
-    detector.SetReadoutConfiguration("EndTop");
     auto* world = detector.Construct();
 
     G4GDMLParser parser;
     parser.SetOutputFileOverwrite(true);
     parser.Write(argv[1], world, true);
-    std::cout << "export_endtop_gdml PASSED: " << argv[1] << '\n';
+    std::cout << "export_endonly_gdml PASSED: " << argv[1] << '\n';
     return EXIT_SUCCESS;
+#else
+    (void)argc;
+    (void)argv;
+    return EXIT_FAILURE;
+#endif
 }
